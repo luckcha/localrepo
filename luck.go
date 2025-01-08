c8ea4588b13fb16dd5b78ce225c6fdd70ce6107a
luckcha <radheyy1805@gmail.com>
2025-01-09 00:28:25 +0530

    go_file

---
+++ luck.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Lucky choudhary"
+	var address string = "Ghaziabad"
+	var Phone_number int = 7302557255
+	var height float64 = 5.10
+	var minor bool = false
+	fmt.Println(name)
+	fmt.Println(address)
+	fmt.Println(Phone_number)
+	fmt.Println(height)
+	fmt.Println(minor)
+
+}
